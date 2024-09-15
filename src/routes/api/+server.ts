40d6ba32a6d0b58d59185ccca76bdd3631b48d0f
Dmytro Tymoshyk <dtymoshyk@360insights.com>
2024-09-15 23:25:36 +0300

    minor fix

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,25 +1,22 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { KVNamespace } from '@cloudflare/workers-types'; // Імпорт типу
 
-// Використовуємо KV_NAMESPACE для продакшену та змінну оточення для локального середовища
 const KV_NAMESPACE: KVNamespace = process.env.NODE_ENV === 'production'
-    ? (globalThis as any).KV_NAMESPACE // Для Cloudflare Workers
-    : { // Для локального середовища, якщо ви бажаєте використати фейковий або реальний API
-        get: async () => null, // Можна додати локальну реалізацію
+    ? (globalThis as any).KV_NAMESPACE
+    : {
+        get: async () => null,
         put: async () => { console.log('Фейковий запис у KV'); }
     } as unknown as KVNamespace;
 
 export const POST: RequestHandler = async ({ request }) => {
     const { url, shortUrl } = await request.json();
 
-    // Отримання значення з KV
     const existingShortUrl = await KV_NAMESPACE.get(shortUrl);
 
     if (existingShortUrl) {
         return new Response(JSON.stringify({ message: 'Short URL already exists' }), { status: 400 });
     }
 
-    // Збереження нового URL в KV
     await KV_NAMESPACE.put(shortUrl, url);
 
     return new Response(null, { status: 200 });
